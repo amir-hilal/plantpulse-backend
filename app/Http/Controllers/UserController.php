e892b0c68fbc3ef385c7ffecf796eb2ae868a4f8
amirhilal044 <amiramirhilal@gmail.com>
2024-08-30 17:49:19 +0300

    added update function to update use data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -16,5 +18,39 @@ class UserController extends Controller
         }
 
         return response()->json($user);
+    }
+    public function update(Request $request, $username)
+    {
+        $user = User::where('username', $username)->firstOrFail();
+
+        if ($user->id !== Auth::id()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'phone_number' => 'nullable|string|max:20',
+            'address' => 'nullable|string|max:255',
+            'birthday' => 'nullable|date',
+            'gender' => 'nullable|string|in:Male,Female,Other',
+            'about' => 'nullable|string|max:1000',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user->update([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'phone_number' => $request->phone_number,
+            'address' => $request->address,
+            'birthday' => $request->birthday,
+            'gender' => $request->gender,
+            'about' => $request->about,
+        ]);
+
+        return response()->json(['message' => 'Profile updated successfully', 'user' => $user], 200);
     }
 }
