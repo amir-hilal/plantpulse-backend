1091d750ed0c12b778d3b7e9f19147b693a246c1
amirhilal044 <amiramirhilal@gmail.com>
2024-09-02 15:20:08 +0300

    added sendRequest method in FriendController

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Friend;
+use Illuminate\Support\Facades\Auth;
 
 class FriendController extends Controller
 {
-    //
+    public function sendRequest(Request $request)
+    {
+        $request->validate([
+            'friend_id' => 'required|exists:users,id',
+        ]);
+
+        $friend = Friend::create([
+            'user_id' => Auth::id(),
+            'friend_id' => $request->friend_id,
+            'status' => 'pending',
+        ]);
+
+        return response()->json(['message' => 'Friend request sent.', 'friend' => $friend], 201);
+    }
 }
