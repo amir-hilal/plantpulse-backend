e9750dbe2dab74068d5f39ffeaef620092bdd01d
amirhilal044 <amiramirhilal@gmail.com>
2024-09-02 15:18:29 +0300

    created friend model

---
+++ app/Models/Friend.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Friend extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['user_id', 'friend_id', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function friend()
+    {
+        return $this->belongsTo(User::class, 'friend_id');
+    }
+}
