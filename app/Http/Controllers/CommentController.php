6dca23b76dd008fdaef06dae58aff93b793433da
amirhilal044 <amiramirhilal@gmail.com>
2024-09-05 18:14:37 +0300

    added fetchComments in CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\PostComment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function fetchComments($postId, Request $request)
+    {
+        $perPage = 10; // Number of comments per page
+        $comments = PostComment::where('post_id', $postId)
+            ->with('user')
+            ->paginate($perPage);
+
+        return response()->json($comments);
+    }
 }
