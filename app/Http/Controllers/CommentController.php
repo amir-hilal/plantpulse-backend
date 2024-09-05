eb38d5b662783ae27df2b2c5d69cd807c18c59b7
amirhilal044 <amiramirhilal@gmail.com>
2024-09-05 19:04:08 +0300

    added addComment method to CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\PostComment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
@@ -14,5 +15,26 @@ class CommentController extends Controller
             ->paginate($perPage);
 
         return response()->json($comments);
+    }
+
+    public function addComment($postId, Request $request)
+    {
+        $request->validate([
+            'comment_text' => 'required|string|max:1000',
+        ]);
+
+        $comment = PostComment::create([
+            'post_id' => $postId,
+            'user_id' => Auth::id(),
+            'comment_text' => $request->comment_text,
+        ]);
+
+        // Load the user details for the comment
+        $comment->load('user:id,first_name,last_name,profile_photo_url');
+
+        return response()->json([
+            'message' => 'Comment added successfully',
+            'comment' => $comment
+        ], 201);
     }
 }
