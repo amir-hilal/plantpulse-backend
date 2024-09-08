35d74216234dc10bc88a06c4fc5da473f8aa2a78
amirhilal044 <amiramirhilal@gmail.com>
2024-09-08 22:31:29 +0300

    created plant timeline model

---
+++ app/Models/PlantTimeline.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlantTimeline extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'plant_id',
+        'description',
+        'image_path',
+    ];
+
+    // Relationship: a timeline belongs to a plant
+    public function plant()
+    {
+        return $this->belongsTo(Plant::class);
+    }
+}
