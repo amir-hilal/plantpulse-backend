00142ed80502cfd58256b94145dd941b644d407c
amirhilal044 <amiramirhilal@gmail.com>
2024-09-12 15:35:23 +0300

    created tutorials and tutorial_comments migrations

---
+++ database/migrations/2024_09_12_122849_create_tutorials_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tutorials', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('video_url');
+            $table->string('thumbnail_url');
+            $table->json('tags')->nullable();  // Tags as JSON array
+            $table->unsignedBigInteger('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tutorials');
+    }
+};
