4e7d118a7834faa7696a8814c07c37a5993fddf8
amirhilal044 <amiramirhilal@gmail.com>
2024-09-18 08:03:58 +0300

    updated tutorials and tutorial comments factories and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,8 @@ use App\Models\PostComment;
 use App\Models\CommunityPost;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -28,5 +30,23 @@ class DatabaseSeeder extends Seeder
         // $this->call(PlantTimelinesTableSeeder::class);
         $this->call(TutorialSeeder::class);
         $this->call(TutorialCommentSeeder::class);
+        User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'User',
+            'username' => 'admin_user',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'profile_photo_url' => fake()->imageUrl(400, 400, 'people', true, 'Faker'),
+            'cover_photo_url' => fake()->optional()->imageUrl(1200, 400, 'nature', true, 'Faker'),
+            'about' => 'Administrator of the system',
+            'phone_number' => fake()->optional()->phoneNumber(),
+            'gender' => 'male',
+            'birthday' => fake()->optional()->date(),
+            'address' => fake()->optional()->address(),
+            'google_id' => fake()->optional()->uuid(),
+            'role' => 'admin',  // Role is set to 'admin'
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
